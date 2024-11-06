c41115e707e3d62fbe7bc3498419b4160b2cc256
midulislamrasel <midulraddy121@gmail.com>
2024-11-06 12:30:43 +0600

    add

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,19 +1,17 @@
-import Image from "next/image";
-import prisma from "./prisma/client";
+import React from "react";
+import prisma from "../prisma/client";
 
-export default function Home() {
+export default function page() {
   const formHandes = async (formData) => {
     "use server";
 
     const name = formData.get("name");
     const email = formData.get("email");
-    const message = formData.get("message");
 
-    const data = await prisma.Contact.create({
+    const data = await prisma.User.create({
       data: {
         name,
         email,
-        message,
       },
     });
 
@@ -26,7 +24,7 @@ export default function Home() {
         action={formHandes}
         className="max-w-lg mx-auto bg-white p-6 rounded-lg shadow-md"
       >
-        <h2 className="text-2xl font-semibold mb-4 text-center">Contact Us</h2>
+        <h2 className="text-2xl font-semibold mb-4 text-center">Login</h2>
         <div className="mb-4">
           <label className="block text-gray-700">Name</label>
           <input
@@ -44,15 +42,6 @@ export default function Home() {
             name="email"
             className="w-full p-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
             placeholder="Your Email"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700">Message</label>
-          <textarea
-            name="message"
-            className="w-full p-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-            placeholder="Your Message"
             required
           />
         </div>
